d9a81f88f29fffec7d3b24f7d64d8ee302f30704
Paul Green <greenpj@yahoo.com>
2013-06-12 09:50:43 +1000

    1

---
+++ DevExpressMDIRibbonMerge/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Ribbon;
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Layout.Core;
+using DevExpress.Xpf.Docking;
+
+
+namespace DXWPFApplication1
+{
+    public partial class MainWindow : DXRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+
+
+}
